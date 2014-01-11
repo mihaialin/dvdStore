7334c01ecd417e6e049937c97b79f62599da592b
mihaialin <mihaialin@wildmedia.ro>
2014-01-11 15:31:48 +0200

    Built
    First and last built

---
+++ dvdStore/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace dvdStore
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dvdTitleBox = new System.Windows.Forms.TextBox();
+            this.releaseYear = new System.Windows.Forms.TextBox();
+            this.publisher = new System.Windows.Forms.TextBox();
+            this.viewerRating = new System.Windows.Forms.TextBox();
+            this.newLanguage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.languageList = new System.Windows.Forms.ListBox();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.previousButton = new System.Windows.Forms.Button();
+            this.modifyButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.load_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dvdTitleBox
+            // 
+            this.dvdTitleBox.Location = new System.Drawing.Point(27, 51);
+            this.dvdTitleBox.Name = "dvdTitleBox";
+            this.dvdTitleBox.Size = new System.Drawing.Size(135, 20);
+            this.dvdTitleBox.TabIndex = 0;
+            // 
+            // releaseYear
+            // 
+            this.releaseYear.Location = new System.Drawing.Point(27, 114);
+            this.releaseYear.Name = "releaseYear";
+            this.releaseYear.Size = new System.Drawing.Size(135, 20);
+            this.releaseYear.TabIndex = 1;
+            // 
+            // publisher
+            // 
+            this.publisher.Location = new System.Drawing.Point(27, 177);
+            this.publisher.Name = "publisher";
+            this.publisher.Size = new System.Drawing.Size(135, 20);
+            this.publisher.TabIndex = 2;
+            // 
+            // viewerRating
+            // 
+            this.viewerRating.Location = new System.Drawing.Point(27, 242);
+            this.viewerRating.Name = "viewerRating";
+            this.viewerRating.Size = new System.Drawing.Size(135, 20);
+            this.viewerRating.TabIndex = 3;
+            // 
+            // newLanguage
+            // 
+            this.newLanguage.Location = new System.Drawing.Point(456, 51);
+            this.newLanguage.Name = "newLanguage";
+            this.newLanguage.Size = new System.Drawing.Size(135, 20);
+            this.newLanguage.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "DVD Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Release Year";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Publisher";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 226);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Viewer Rating";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(453, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "New Language";
+            // 
+            // languageList
+            // 
+            this.languageList.FormattingEnabled = true;
+            this.languageList.Location = new System.Drawing.Point(223, 51);
+            this.languageList.Name = "languageList";
+            this.languageList.Size = new System.Drawing.Size(165, 212);
+            this.languageList.TabIndex = 10;
+            this.languageList.SelectedIndexChanged += new System.EventHandler(this.languageList_SelectedIndexChanged);
+            // 
+            // nextButton
+            // 
+            this.nextButton.Location = new System.Drawing.Point(516, 310);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(75, 23);
+            this.nextButton.TabIndex = 11;
+            this.nextButton.Text = ">>";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // previousButton
+            // 
+            this.previousButton.Location = new System.Drawing.Point(426, 310);
+            this.previousButton.Name = "previousButton";
+            this.previousButton.Size = new System.Drawing.Size(75, 23);
+            this.previousButton.TabIndex = 12;
+            this.previousButton.Text = "<<";
+            this.previousButton.UseVisualStyleBackColor = true;
+            this.previousButton.Click += new System.EventHandler(this.previousButton_Click);
+            // 
+            // modifyButton
+            // 
+            this.modifyButton.Location = new System.Drawing.Point(456, 88);
+            this.modifyButton.Name = "modifyButton";
+            this.modifyButton.Size = new System.Drawing.Size(135, 23);
+            this.modifyButton.TabIndex = 13;
+            this.modifyButton.Text = "Modify";
+            this.modifyButton.UseVisualStyleBackColor = true;
+            this.modifyButton.Click += new System.EventHandler(this.modifyButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(426, 269);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(165, 23);
+            this.saveButton.TabIndex = 14;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // load_button
+            // 
+            this.load_button.Location = new System.Drawing.Point(426, 239);
+            this.load_button.Name = "load_button";
+            this.load_button.Size = new System.Drawing.Size(165, 23);
+            this.load_button.TabIndex = 15;
+            this.load_button.Text = "Load";
+            this.load_button.UseVisualStyleBackColor = true;
+            this.load_button.Click += new System.EventHandler(this.load_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 345);
+            this.Controls.Add(this.load_button);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.modifyButton);
+            this.Controls.Add(this.previousButton);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.languageList);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.newLanguage);
+            this.Controls.Add(this.viewerRating);
+            this.Controls.Add(this.publisher);
+            this.Controls.Add(this.releaseYear);
+            this.Controls.Add(this.dvdTitleBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox dvdTitleBox;
+        private System.Windows.Forms.TextBox releaseYear;
+        private System.Windows.Forms.TextBox publisher;
+        private System.Windows.Forms.TextBox viewerRating;
+        private System.Windows.Forms.TextBox newLanguage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox languageList;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Button previousButton;
+        private System.Windows.Forms.Button modifyButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button load_button;
+    }
+}
+
---
+++ dvdStore/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace dvdStore
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static dvd[] dvdList = new dvd[5];
+        public static int listIndex = 0;
+        public static int maxIndex = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                      
+        }
+
+        public class dvd { 
+            private String dvdTitle;
+            private int releaseYear;
+            private String publisher;
+            private double viewerRating;
+            private String[] languages = new String[3];
+
+            public String getDvdTitle() {
+                return dvdTitle;
+            }
+            public int getReleaseYear() {
+                return releaseYear;
+            }
+            public String getPublisher() {
+                return publisher;
+            }
+            public double getViewerRating() {
+                return viewerRating;
+            }
+            public String[] getLanguages() {
+                return languages;
+            }
+            public string getLanguageAtIndex(int index) {
+                return languages[index];
+            }
+            public string getSerializedLanguages() {
+                return languages[0] + "," + languages[1]; 
+            }
+            public void setLanguageAtIndex(int index, string lang) {
+                languages[index] = lang;
+            }
+            public void setDvdTitle(String title) {
+                dvdTitle = title;
+            }
+            public void setReleaseYear(int year) {
+                releaseYear = year;
+            }
+            public void setPublisher(String pub) {
+                publisher = pub;
+            }
+            public void setViewerRating(double vr) {
+                viewerRating = vr;
+            }
+            public void setLanguages(String[] lang){
+                languages = lang;
+            }
+
+        }
+
+        private void load_button_Click(object sender, EventArgs e)
+        {
+            
+            
+            string outPath = @"C:\Facultate\dvdStore.txt";
+            FileStream inputFile = new FileStream(outPath, FileMode.Open, FileAccess.Read);
+            StreamReader inStream = new StreamReader(inputFile);
+
+            string line;
+            int counter = 0;
+            while ((line = inStream.ReadLine()) != null)
+            {
+                dvdList[counter] = new dvd();
+                string[] dvdData = line.Split(';');
+                dvdList[counter].setDvdTitle(dvdData[0]);
+                dvdList[counter].setReleaseYear(Convert.ToInt32(dvdData[1]));
+                dvdList[counter].setPublisher(dvdData[2]);
+                dvdList[counter].setViewerRating(Convert.ToDouble(dvdData[3]));
+                String[] langs = dvdData[4].Split(',');
+                dvdList[counter].setLanguages(langs);
+                maxIndex = counter;
+                counter++;
+                
+            }
+            displayValues();
+            inStream.Close();
+            inputFile.Close();
+        }
+
+        public void displayValues() {
+            languageList.Items.Clear();
+            dvdTitleBox.Text = dvdList[listIndex].getDvdTitle();
+            releaseYear.Text = (dvdList[listIndex].getReleaseYear()).ToString();
+            publisher.Text = dvdList[listIndex].getPublisher();
+            viewerRating.Text = (dvdList[listIndex].getViewerRating()).ToString();
+            foreach(string item in dvdList[listIndex].getLanguages()){
+                languageList.Items.Add(item);
+            }
+
+        }
+
+        private void previousButton_Click(object sender, EventArgs e)
+        {
+            if (listIndex > 0) {
+                listIndex--;
+            }
+            
+            displayValues();
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            if (listIndex < maxIndex)
+            {
+                listIndex++;
+            }
+            
+            displayValues();
+        }
+
+        private void languageList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int newLangIndex = Convert.ToInt32(languageList.GetItemText(languageList.SelectedIndex));
+            newLanguage.Text = dvdList[listIndex].getLanguageAtIndex(newLangIndex);
+        }
+
+        private void modifyButton_Click(object sender, EventArgs e)
+        {
+         
+            int newLangIndex = Convert.ToInt32(languageList.GetItemText(languageList.SelectedIndex));
+            if (newLangIndex >= 0) {
+                dvdList[listIndex].setLanguageAtIndex(newLangIndex, newLanguage.Text);
+                dvdList[listIndex].setDvdTitle(dvdTitleBox.Text);
+            }
+          
+            if (((Convert.ToInt32(releaseYear.Text) < 1912) || (Convert.ToInt32(releaseYear.Text) > 2014)))
+            {
+                System.Windows.Forms.MessageBox.Show("Invalid year");
+            }
+            else {
+                dvdList[listIndex].setReleaseYear(Convert.ToInt32(releaseYear.Text));
+            }
+            
+            dvdList[listIndex].setPublisher(publisher.Text);
+            dvdList[listIndex].setViewerRating(Convert.ToDouble(viewerRating.Text));
+
+            displayValues();
+        }
+
+        public void saveDVDList() {
+            string outPath = @"C:\Facultate\dvdStore.txt";
+            FileStream inputFile = new FileStream(outPath, FileMode.Open, FileAccess.Write);
+            StreamWriter inStream = new StreamWriter(inputFile);
+            inputFile.Flush();
+            int i=0;
+            while (i < maxIndex) {
+                inStream.WriteLine(dvdList[i].getDvdTitle() + ";" + (dvdList[i].getReleaseYear()).ToString() + ";" + dvdList[i].getPublisher() + ";" + (dvdList[i].getViewerRating()).ToString() + ";" + dvdList[i].getSerializedLanguages());
+                i++;
+            }
+            inStream.Close();
+            inputFile.Close();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            saveDVDList();
+        }
+    }
+}
